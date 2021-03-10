12d53178323846f21f36ca87c7cbebb2a9e8b3c1
= <=>
2021-03-10 13:54:20 +0530

    ...

---
+++ javascript/linked-list/doubly.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(data, next, prev) {
+    this.data = data;
+    this.next = next;
+    this.prev = next;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    //create null initialized list
+    this.list = null;
+  }
+}
