1585bc0e17933af915f4b2f2346b401e771f8ca8
= <=>
2021-03-10 21:16:47 +0530

    stack using array added

---
+++ javascript/stack/stack_arr.js
@@ -0,0 +1,52 @@
+class Stack {
+  constructor(capacity) {
+    this.MAX_SIZE = capacity;
+    this.arr = new Array(capacity);
+    this.CURRENT_SIZE = 0;
+  }
+
+  push = (data) => {
+    if(this.CURRENT_SIZE < this.MAX_SIZE) {
+        this.arr[this.CURRENT_SIZE] = data;
+        this.CURRENT_SIZE += 1;
+        console.log(`${data} added to stack`);
+    } else {
+        console.error("stack is full");
+    }
+  };
+
+  pop = () => {
+    if(this.CURRENT_SIZE > 0) {
+        let index = this.CURRENT_SIZE;
+        this.CURRENT_SIZE -= 1;
+        return this.arr[index];
+    } else {
+        console.error("stack is empty");
+    }
+  };
+
+  display = () => {
+    for(let i = 0; i < this.CURRENT_SIZE; i++) {
+        console.log(this.arr[i]);
+    }
+  };
+}
+
+//staring point of script
+const main = () => {
+  const stack = new Stack(5);
+  stack.push(1);
+  stack.push(2);
+  stack.push(3);
+  stack.push(4);
+  stack.push(5);
+  stack.display();
+  console.log("pop");
+  stack.pop();
+  stack.pop();
+  stack.pop();
+  stack.display();
+};
+
+//run program
+main();
