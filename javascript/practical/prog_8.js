a2ea64bd9954304c64dc78d51d7de7e797348395
= <=>
2021-04-16 00:35:37 +0530

    program-8 reverse string using stack added

--- javascript/practical/prog_8.js
+++ javascript/practical/prog_8.js
@@ -1,4 +1,9 @@
-//Q-8 = Write  a program  to Reverse  a string  using  stack.
+/*
+* Q-8 Write  a program  to Reverse  a string  using  stack.
+* Sandeep mishra
+* MCA 1st year (sem-1)
+* Roll No. 2012093
+*/
 
 const STACK = []; // empty stack
 let TOP = -1; // holds current position (index) of newly added element.
