0754718403906a97db6e2b159e2bbcff48705848
= <=>
2021-04-16 19:02:28 +0530

    ...

--- javascript/practical/prog_9.js
+++ javascript/practical/prog_9.js
@@ -5,6 +5,7 @@
  * Roll No. 2012093
  */
 
+const parantheses = ["(", ")", "{", "}", "[", "]"];
 const STACK = []; // empty stack
 let TOP = -1; // holds current position (index) of newly added element.
 
@@ -19,4 +20,27 @@ function pop() {
   TOP -= 1;
   return topElement;
 }
-// TODO: implement the rest of the logics....
+
+//TODO: STILL PENDING
+function checkBalance(dataStr) {
+  const strLength = dataStr.length;
+  // create stack of parantheses
+  for (let i = 0; i < strLength; i++) {
+    if (parantheses.includes(dataStr[i])) {
+      push(dataStr[i]);
+    }
+  }
+  // check if length is "EVEN" then there is possibility of balanced parantheses, otherwise not.
+  if (strLength % 2 === 0) {
+    // create stack
+    for (let i = 0; i < strLength; i++) {
+      push(dataStr[i]);
+    }
+  }
+  // this means length is odd and parantheses is not balanced.
+  return "parentheses are **NOT** balanced.";
+}
+
+////////////////////////////////////////////////////////
+const result = checkBalance("(((())))");
+console.log(result);
